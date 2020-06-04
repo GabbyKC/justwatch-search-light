608cef7373efdfab8595b2603705a600ee01ed12
Gabriella Cannon <gabriellakate04@gmail.com>
2020-06-04 20:44:46 +0200

    app setup

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,31 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
-  </div>
+  <main id="app">
+    <SearchLight />
+  </main>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import SearchLight from "./components/SearchLight.vue";
 
 export default {
   name: "App",
   components: {
-    HelloWorld
+    SearchLight
   }
 };
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  * {
+    box-sizing: border-box;
+  }
+
+  #app {
+    font-family: Geneva, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    width: 80%;
+    margin: 2.5rem auto;
+    color: #154e3b;
+  }
 </style>
---
+++ src/components/SearchLight.vue
@@ -0,0 +1,34 @@
+<template>
+  <section class="container">
+    <div class="background">
+      <h1>Search Light</h1>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  name: "SearchLight"
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+  .container {
+    width: 80%;
+    margin: 0 auto;
+  }
+
+  .background {
+    background: url("../assets/logo.jpg") center no-repeat;
+    height: 30rem;
+    display: flex;
+    text-align: center;
+  }
+
+  h1 {
+    font-size: 2rem;
+    color: white;
+    width: 100%;
+  }
+</style>
